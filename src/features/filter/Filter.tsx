c55e6c018118b8355e0f6f49fc76162937a343c8
ivan kevra <vanyakevra@gmail.com>
2023-05-12 14:39:24 +0300

    fix header ui

--- src/features/filter/Filter.tsx
+++ src/features/filter/Filter.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import style from './Filter.module.css'
+import {Button, Select} from '@mantine/core';
+import {IconChevronDown} from '@tabler/icons-react';
 
 export const Filter = () => {
     return (
         <div className={style.filter}>
-            Фильтры
+            <div className={style.header}>
+                <div>Фильтры</div>
+                <div>Сбросить все</div>
+            </div>
+            <div>
+                <Select
+                    label="Отрасль"
+                    placeholder="Выберите отрасль"
+                    rightSection={<IconChevronDown size="1rem"/>}
+                    rightSectionWidth={30}
+                    styles={{rightSection: {pointerEvents: 'none'}}}
+                    data={['React', 'Angular', 'Svelte', 'Vue']}
+                />
+            </div>
+            <div>
+                <Select
+                    label="Оклад"
+                    placeholder="От"
+                    data={[
+                        {value: '100', label: '100'},
+                        {value: '200', label: '200'},
+                        {value: '300', label: '300'},
+                        {value: '400', label: '400'},
+                    ]}
+                />
+                <Select
+                    placeholder="До"
+                    data={[
+                        {value: '100', label: '100'},
+                        {value: '200', label: '200'},
+                        {value: '300', label: '300'},
+                        {value: '400', label: '400'},
+                    ]}
+                />
+                <Button radius="lg" size="md">
+                    Применить
+                </Button>
+            </div>
+
+
         </div>
     );
 };
--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import style from './Style.module.css'
 import logo from '../../assets/img/logo.svg'
+import {Button, Grid, Image, Title} from "@mantine/core";
 
 
 export const Header = () => {
     return (
-        <div>
-            <div className={style.header}>
-                <div className={style.logo}>
-                    <img src={logo} alt={'logo'} className={style.logo}/>
-                    <h1>Jobored</h1>
-                </div>
-                <div className={style.menu}>
-                    <span>Поиск вакансий</span>
-                    <span>Избранное</span>
-                </div>
-
-            </div>
-        </div>
-
+        <Grid justify="center" bg='#ffff' align='center'>
+            <Grid.Col span={4} className={style.logo}>
+                <Image maw={36} src={logo} alt="Logo"/>
+                <Title order={1}>Jobored</Title>
+            </Grid.Col>
+            <Grid.Col span={8} bg='blue' className={style.menu}>
+                <Button variant="subtle" size="16px">
+                    Поиск вакансий
+                </Button>
+                <Button variant="subtle" color="dark" size="16px">
+                    Избранное
+                </Button>
+            </Grid.Col>
+        </Grid>
     );
 };
 
